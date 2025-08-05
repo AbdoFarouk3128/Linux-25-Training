77e7083fec4caab5b61c628382179074f838acb9
AbdoFarouk3128 <20241701056@cis.asu.edu.eg>
2025-08-06 00:57:27 +0300

    Task submission.

---
+++ Task-1/solution.sh
@@ -0,0 +1,15 @@
+pwd
+ls
+cd gate-1/
+rm trap
+cp key gate-2/
+cd gate-2
+mv closed open
+mv open gate-3/
+cd gate-3/
+rm trap
+cd gate-4/
+rm -r dimension-x dimension-y
+cd earth/
+mkdir home
+touch home/bed
